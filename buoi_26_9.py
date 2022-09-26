ddcc07c7f17dc80f017f60560f6ce2c792971c92
Phuocialy <103870778+Phuocialy@users.noreply.github.com>
2022-09-26 21:14:55 +0700

    Update buoi_26_9.py

--- buoi_26_9.py
+++ buoi_26_9.py
@@ -6,16 +6,16 @@ df = pd.read_csv('FoodPrice_in_Turkey.csv')
 
 
 # vẽ biểu đồ cột so sánh giá gạo tháng 12 năm 2019 của Ankara, Istanbul, Izmir và Nation Average
-# data1 = df[(df['Year'] == 2019) & (df['Month'] == 12) & (df['ProductName'] == 'Rice - Retail')]
+data1 = df[(df['Year'] == 2019) & (df['Month'] == 12) & (df['ProductName'] == 'Rice - Retail')]
 # # print(data1)
-# plt.title("Gia gao thang 12 nam 2019")
-# plt.bar(data1['Place'], data1['Price'])
-# plt.show()
+plt.title("Gia gao thang 12 nam 2019")
+plt.bar(data1['Place'], data1['Price'])
+plt.show()
 
 # Vẽ biểu đồ đường phân tích xu hướng giá gạo (Rice-Retail) trung bình cả nước (National Average) trong năm 2019 tại Thổ Nhĩ Kì
-# data2 = df[(df['Place'] == 'National Average') & (df['Year'] == 2019) & (df['ProductName'] == 'Rice - Retail')]
-# plt.plot(data2['Month'], data2['Price'])
-# plt.show()
+data2 = df[(df['Place'] == 'National Average') & (df['Year'] == 2019) & (df['ProductName'] == 'Rice - Retail')]
+plt.plot(data2['Month'], data2['Price'])
+plt.show()
 
 # Vẽ biểu đồ Scatter phân tích mối liên quan giữa giá gạo và giá gas trung bình quốc gia (National Average) tại Thổ Nhĩ Kì
 
