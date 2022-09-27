b8b04bb547b8387373009fa15b71cb21e139f75e
Phuocialy <103870778+Phuocialy@users.noreply.github.com>
2022-09-27 15:32:29 +0700

    Thuc hanh ghi du lieu tu DataFrame

---
+++ Ghi_du_lieu.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv('FoodPrice_in_Turkey.csv')
+print(df.head())
+
+# Ghi dữ liệu từ DataFrame vào file csv
+df.to_csv('demo_FoodPrice.csv')
+
+# Ghi dữ liệu từ DataFrame vào file Json
+df.to_json('demo_FoodPrice.json',orient='columns')
+
+# Ghi dữ liệu từ DataFrame vào file HDF5
+df.to_hdf('demo_FoodPrice.h5', 'table')
+
+# Ghi dữ liệu từ DataFrame vào file HTML
+df.to_html('demo_FoodPrice.html')
